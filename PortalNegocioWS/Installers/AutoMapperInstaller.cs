be6007b41e6eb09d6d764f3ee1956a96e1e9a2f1
Adrian Castro <oscaradrianc@hotmail.com>
2026-04-15 15:24:33 -0500

    feat(01-05): add AssertConfigurationIsValid() to AutoMapper startup validation
    - Added config.AssertConfigurationIsValid() in AutoMapperInstaller.InstallServices()
    - Placed after MapperConfiguration creation and before CreateMapper()
    - Ensures 80+ mappings are validated at startup, failing fast on configuration errors
    - All AutoMapper mappings passed validation during build
    - Requirement HYG-06: Fail fast on mapper configuration issues

--- PortalNegocioWS/Installers/AutoMapperInstaller.cs
+++ PortalNegocioWS/Installers/AutoMapperInstaller.cs
@@ -7,11 +7,13 @@ namespace PortalNegocioWS.Installers
     public class AutoMapperInstaller : IInstaller
     {
         public void InstallServices(IServiceCollection services, IConfiguration configuration)
-        {          
+        {
             var config = new AutoMapper.MapperConfiguration(c =>
             {
                 c.AddProfile(new Mappings.MappingProfile());
             });
+
+            config.AssertConfigurationIsValid();
 
             var mapper = config.CreateMapper();
 
